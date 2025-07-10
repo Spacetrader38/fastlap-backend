4bd95fd2b19b95c2fc95966ee3461d067d44ea02
Spacetrader38 <Spacetrader38@users.noreply.github.com>
2025-07-11 00:26:13 +0200

    fix: suppression du await non autorisé dans orders.js

--- models/orders.js
+++ models/orders.js
@@ -1,16 +1,19 @@
-const Order = require('../models/Order');
+const mongoose = require('mongoose');
 
-await Order.create({
-  email,
-  civilite,
-  nom,
-  prenom,
-  adresse,
-  ville,
-  codePostal,
-  telephone,
-  commande,
-  totalHT: totalHT.toFixed(2),
-  tva: tva.toFixed(2),
-  totalTTC: totalTTC.toFixed(2)
+const orderSchema = new mongoose.Schema({
+  date: Date,
+  email: String,
+  civilite: String,
+  nom: String,
+  prenom: String,
+  adresse: String,
+  ville: String,
+  codePostal: String,
+  telephone: String,
+  commande: Array,
+  totalHT: String,
+  tva: String,
+  totalTTC: String
 });
+
+module.exports = mongoose.model('Order', orderSchema);
